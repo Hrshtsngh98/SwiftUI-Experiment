173eb387242c7a24cfe1a7fefc79a447a5fca074
Harshit Singh <harshitsingh0401@gmail.com>
2026-01-11 17:59:44 -0500

    Initial Commit

---
+++ SwiftUI Experiment/SwiftUI_ExperimentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ExperimentApp.swift
+//  SwiftUI Experiment
+//
+//  Created by Harshit on 1/11/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ExperimentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
